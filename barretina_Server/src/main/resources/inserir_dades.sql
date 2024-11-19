c28af801525d55dcabe324259c710f145b9f8bce
orirribas2000@gmail.com <orirribas2000@gmail.com>
2024-11-19 18:58:22 +0100

    script per inserir dades creat

--- barretina_Server/src/main/resources/inserir_dades.sql
+++ barretina_Server/src/main/resources/inserir_dades.sql
@@ -1,13 +1,21 @@
-insert into cambrer(nom) VALUES (("Pepe"),("Juan"),("Pedro"));
+use barretina;
+insert into cambrer(nom) VALUES ("Pepe"),("Juan"),("Pedro");
 
-Create procedure insertTaules (inici int , final int)
-    Begin 
-        DECLARE c int default inici;
-        DECLARE idcambrer int;
-        SELECT id_cambrer INTO idcambrer FROM cambrer ORDER BY id_cambrer DESC LIMIT 1; 
-        
-        WHILE c <= final DO
-            insert into taula (id_cambrer,ocupada) VALUES (idcambrer,FALSE);
-            set c = c + 1;
-        END WHILE; 
-    END;
+DELIMITER $$
+
+CREATE PROCEDURE insertTaules (inici INT, final INT)
+BEGIN
+    DECLARE c INT DEFAULT 0;
+    DECLARE idcambrer INT;
+
+    SET c = inici;
+    SELECT id_cambrer INTO idcambrer FROM cambrer ORDER BY id_cambrer DESC LIMIT 1;
+
+    WHILE c <= final DO
+        INSERT INTO taula (id_cambrer, ocupada) VALUES (idcambrer, FALSE);
+        SET c = c + 1;
+    END WHILE;
+END$$
+
+DELIMITER ;
+
