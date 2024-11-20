286e984fcd00078081e9192dcb04d98fc32abdf7
Oriol Arribas <152431522+slenderrap@users.noreply.github.com>
2024-11-20 21:27:20 +0100

    trigger creat i dades inserides, s'ha modificat l'script de la bbdd

--- barretina_Server/src/main/resources/bbdd_barretina.sql
+++ barretina_Server/src/main/resources/bbdd_barretina.sql
@@ -3,7 +3,7 @@ create table `producte`(
     `id_producte` INT(3) NOT NULL PRIMARY KEY auto_increment,
     `nom` VARCHAR(30) NOT NULL,
     `calent` BOOLEAN,
-    `preu` DECIMAL(2,2)
+    `preu` DECIMAL(4,2)
 );
 create table `cambrer`(
     `id_cambrer` INT(2) NOT NULL PRIMARY KEY auto_increment,
@@ -18,7 +18,7 @@ create table `taula`(
 create table `comanda`(
     `id_comanda` INT(3) NOT NULL PRIMARY KEY auto_increment,
     `id_taula` INT(2) NOT NULL ,
-    `preu_total` DECIMAL(5,2),
+    `preu_total` DECIMAL(6,2),
     `pagat` BOOLEAN,
     `data` DATETIME,
     FOREIGN KEY (`id_taula`) REFERENCES `taula`(`id_taula`)
@@ -27,7 +27,7 @@ create table `comanda_producte`(
     `id_comanda` INT(3) NOT NULL,
     `id_producte` INT(2) NOT NULL,
     `quantitat` INT(2) DEFAULT 1,
-    `preu_conjunt` DECIMAL(4,2),
+    `preu_conjunt` DECIMAL(7,2),
     `estat` enum('demanat','preparacio','llest','pagat'),
     PRIMARY KEY (`id_comanda`, `id_producte`),
     FOREIGN KEY (`id_comanda`) REFERENCES `comanda`(`id_comanda`),
--- barretina_Server/src/main/resources/inserir_dades.sql
+++ barretina_Server/src/main/resources/inserir_dades.sql
@@ -18,4 +18,48 @@ BEGIN
 END$$
 
 DELIMITER ;
+insert into producte(nom, calent, preu, tipus) values
+("Coca Cola",0,1.50,"beguda"),
+("Cervesa Corona",0,2.00,"beguda"),
+("Fanta de Taronja",0,2.50,"beguda"),
+("Aigua mineral",0,1.20,"beguda"),
+("Vermut",0,2.80,"beguda"),
+("Suc de pinya",0,2.20,"beguda"),
+("Cafe",1,1.30,"beguda"),
 
+("Patates braves",1,4.50,"tapa"),
+("Croquetes de pernil",1,5.00,"tapa"),
+("Calamars a la romana",1,6.50,"tapa"),
+("Pernil iberic ",0,12.00,"tapa"),
+("Bombes picants",1,5.50,"tapa"),
+("Xampinyons a l'allet",1,4.80,"tapa"),
+
+("Crema catalana",0,4.00,"postre"),
+("Mel i mato",0,4.50,"postre"),
+("Pastis de formatge",0,4.80,"postre"),
+("Flam d'ou",0,3.50,"postre"),
+("Brownie amb gelat",1,5.00,"postre"),
+("Maduixes amb nata",0,3.50,"postre"),
+
+("Amanida catalana",0,7.50,"entrant"),
+("Sopa de galets",1,6.00,"entrant"),
+("Escalivada",0,6.50,"entrant"),
+("Canelons",1,8.50,"entrant"),
+("Macarrons a la bolonyesa ",1,3.00,"entrant"),
+("Gaspatxo",0,5.00,"entrant");
+
+DELIMITER $$
+
+CREATE TRIGGER actualizar_preu_conjunt
+AFTER UPDATE ON comanda_producte
+FOR EACH ROW
+BEGIN
+    -- Solo recalcula preu_conjunt cuando la columna quantitat ha cambiado
+    IF OLD.quantitat <> NEW.quantitat THEN
+        UPDATE comanda_producte
+        SET preu_conjunt = (SELECT preu FROM producte WHERE id_producte = NEW.id_producte) * NEW.quantitat
+        WHERE id_producte = NEW.id_producte and id_comanda= NEW.id_comanda;
+    END IF;
+END$$
+
+DELIMITER ;
